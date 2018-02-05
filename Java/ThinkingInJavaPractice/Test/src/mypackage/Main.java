043e9ae439dff3bb4bf5922d2be0fe83a06236bc
hunterteaegg-HOME <1009468031@qq.com>
2018-02-05 19:28:28 +0800

    New

---
+++ Java/ThinkingInJavaPractice/Test/src/mypackage/Main.java
@@ -0,0 +1,24 @@
+/**
+ * @author hunterteaegg
+ */
+package mypackage;
+
+/**
+ * @author HunterTeaEgg_HOME
+ *
+ */
+public class Main {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO 自动生成的方法存根
+		for(int i=0;i<10;i++)
+		{
+			System.out.println("You are the best!");
+		}
+
+	}
+
+}
