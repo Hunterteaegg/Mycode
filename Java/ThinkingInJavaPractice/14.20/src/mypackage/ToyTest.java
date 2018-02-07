85e0b957e56f8e9e3a07f40bd91c59a93f85a727
hunterteaegg-HOME <1009468031@qq.com>
2018-02-08 07:55:02 +0800

    continue practice

--- Java/ThinkingInJavaPractice/14.20/src/mypackage/ToyTest.java
+++ Java/ThinkingInJavaPractice/14.20/src/mypackage/ToyTest.java
@@ -33,8 +33,8 @@ public class ToyTest {
     print("Canonical name : " + cc.getCanonicalName());
   }
   public static void main(String[] args) throws InstantiationException, IllegalAccessException, IllegalArgumentException, ReflectiveOperationException {
-    Class c=Class.forName("mypackage.Toy");
-    Constructor constructors=c.getConstructor(String.class);
+    Class<?> c=Class.forName("mypackage.Toy");
+    Constructor<?> constructors=c.getConstructor(String.class);
     Toy toy=(Toy)constructors.newInstance("s");
     System.out.println(toy);
 }
