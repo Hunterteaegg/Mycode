1b3ef7744cabd33d75d36d67c25e9503cf18b2ad
hunterteaegg-HOME <1009468031@qq.com>
2018-02-07 22:30:38 +0800

    Reflect

--- Java/ThinkingInJavaPractice/Test/src/mypackage/Main.java
+++ Java/ThinkingInJavaPractice/Test/src/mypackage/Main.java
@@ -3,6 +3,9 @@
  */
 package mypackage;
 
+import java.lang.reflect.Constructor;
+import java.lang.reflect.Method;
+
 /**
  * @author HunterTeaEgg_HOME
  *
@@ -14,11 +17,28 @@ public class Main {
 	 */
 	public static void main(String[] args) {
 		// TODO 自动生成的方法存根
-		for(int i=0;i<10;i++)
+		Class c=MyClass.class;
+		Method[] methods=c.getMethods();
+		Constructor[] constructors=c.getConstructors();
+		
+		for(Method method:methods)
 		{
-			System.out.println("You are the best!");
+			System.out.println(method);
+		}
+		System.out.println();
+		for(Constructor constructor:constructors)
+		{
+			System.out.println(constructor);
 		}
 
 	}
 
+}
+
+class MyClass
+{
+	public void print()
+	{
+		System.out.println("Hello World!");
+	}
 }
