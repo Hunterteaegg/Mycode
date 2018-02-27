ee371e5f0ac7a0ccc9fd76d7a8e1370676cb1c0a
hunterteaegg-HOME <1009468031@qq.com>
2018-02-27 22:29:35 +0800

    homework?

--- Java/ThinkingInJavaPractice/MyTestNew/src/mypackage/Main.java
+++ Java/ThinkingInJavaPractice/MyTestNew/src/mypackage/Main.java
@@ -1,19 +1,16 @@
 package mypackage;
 
-import java.io.File;
-import java.net.URI;
-import java.net.URISyntaxException;
+import java.util.Scanner;
 
 public class Main {
 
-	public static void main(String[] args) throws URISyntaxException {
+	public static void main(String[] args){
 		// TODO 自动生成的方法存根
-		File file=new File(new URI("https://www.baidu.com"));
-		
-		for(String s:file.list())
-		{
-			System.out.println(s);
-		}
+		System.out.println("Enter a number:");
+		Scanner scanner=new Scanner(System.in);
+		int i=scanner.nextInt();
+		scanner.close();
+		System.out.println("The number is "+i);
 
 	}
 
--- R/RProject1/RProject1/Script.R
+++ R/RProject1/RProject1/Script.R
@@ -1,3 +1,3 @@
-year= c(1, 2, 3, 4, 5);
-data = c(2, 4, 6, 8, 10);
-plot(x, y, type = "o", xlab = "年份", ylab = "数据",col = "black", main = "测试", pch = c(17));
+year= c(2012, 2013, 2014, 2015, 2016);
+data = c(7.9, 7.8, 7.3, 6.9, 6.7);
+plot(year, data, type = "o", xlab = "年份", ylab = "国民生产总值增速（百分比）",col = "black", main = "最近五年的国民生产总值增速", pch = c(17));
